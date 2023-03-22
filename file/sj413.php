58d1e4604c2e67085898f1f42ba0aae89e81a4a0
Hwangtaehun <r283@naver.com>
2023-03-22 19:35:05 +0900

    Update sj413.php

--- file/sj413.php
+++ file/sj413.php
@@ -2,7 +2,7 @@
 <html lang="ko">
   <head>
     <meta charset="utf-8"><title>메모장</title>
-    <link rel = "stylesheet" href="..\css\form.css" />
+    <link rel = "stylesheet" href="..\css\form2.css" />
   </head>
   <body>
     <header>
