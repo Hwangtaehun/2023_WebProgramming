40fba8b2dd2e8564c6d79a0f52120704683b8288
Hwangtaehun <r283@naver.com>
2023-04-05 17:34:16 +0900

    일부 완성
    입력 삭제 수정 부분 만들어보기

--- phpmysql/sj6index.php
+++ phpmysql/sj6index.php
@@ -1,11 +1,32 @@
 <?php
+// $title = '성적 관리';
+// ob_start();
+// include __DIR__.'/../templates/sj6home.html.php';
+// $outString  = ob_get_clean();'
+try{
+    include_once __DIR__.'/../includes/Dbconnect.php';
+    include_once __DIR__.'/../class/TableManager.php';
+    include_once __DIR__.'/../controllers/studentController.php';
 
-$title = '성적 관리';
+    $stuTable = new TableManager($pdo, 'student', 'stu_no');
+    $scoreTable = new TableManager($pdo, 'score', 'sc_id');
+    $stuController = new StudentController($stuTable, $scoreTable);
 
-ob_start();
+    if(isset($_GET['add'])){
+        $page = $stuController->scoreAddUpdate();
+    }elseif(isset($_GET['del'])){
+        $page = $stuController->scoreDelete();
+    }elseif(isset($_GET['list'])){
+        $page = $stuController->scoreList();
+    }else {
+        $page = $stuController->home();
+    }
 
-include __DIR__.'/../templates/sj6home.html.php';
+    $title = $page['title'];
+    ob_start();
+    $outString = $page['outString'];
 
-$outString  = ob_get_clean();
-
+}catch(Exception $ex){
+    $outString='<p>오류발생:'.$e->getMessage().$e->getFile().'행:'.$e->getLine().'</p>';
+}
 include __DIR__.'/../templates/sj6layout.html.php';
--- templates/sj6layout.html.php
+++ templates/sj6layout.html.php
@@ -14,8 +14,10 @@
                 <li><a href="sj6index.php">홈</a></li>
                 <li><a href="sj6stuList.php">수강생 현황</a></li>
                 <li><a href="sj6stuAddUpdate.php">수강생 입력</a></li>
-                <li><a href="sj6scoreList.php">점수 보기</a></li>
-                <li><a href="sj6scoreAddUpdate.php">점수 입력</a></li>
+                <li><a href="sj6index.php?list">점수 보기</a></li>
+                <li><a href="sj6index.php?add">점수 입력</a></li>
+                <!-- <li><a href="sj6scoreList.php">점수 보기</a></li>
+                <li><a href="sj6scoreAddUpdate.php">점수 입력</a></li> -->
             </ul>
         </nav>
         <main>
